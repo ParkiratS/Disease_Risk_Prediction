81bc75e3680dd6191929ce9e902eba97767b3a91
Parkirat Sandhu <parkiratsandhu1@gmail.com>
2022-08-19 18:07:33 -0400

    Get multiple images from GMaps Api

--- test.py
+++ test.py
@@ -15,16 +15,18 @@ center = (results[0]['geometry']['lat'] , results[0]['geometry']['lng'])
 
 
 api_key = "AIzaSyBwVPcm0csBOu_qgM0P4ruhXihSNojUF8I"
-  
+zoom = 19
 
 url = "https://maps.googleapis.com/maps/api/staticmap?"
   
 # get method of requests module
 # return response object
-r = requests.get('https://maps.googleapis.com/maps/api/staticmap?center=' +str(center[0]) + ',' + str(center[1]) + '&zoom=19&size=1000x1000&maptype=satellite&format=png8&key=AIzaSyBwVPcm0csBOu_qgM0P4ruhXihSNojUF8I')
   
 # wb mode is stand for write binary mode
-
-with open('test.png', 'wb') as file:
-   # writing data into the file
-   file.write(r.content)
+for i in range(200):
+   r = requests.get('https://maps.googleapis.com/maps/api/staticmap?center=' +str(center[0]) + ',' + str(center[1]) + '&zoom=' + str(zoom) + '&size=1000x1000&maptype=satellite&format=png8&key='+api_key)
+   with open('Images/test.png'+str(i), 'wb') as file:
+      # writing data into the file
+      print('Getting image: ' + str(i+1))
+      file.write(r.content)
+      center = (center[0]+0.1, center[1]+0.1)
